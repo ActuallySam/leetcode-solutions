223a518331f83e1c4762f5c8d88309c45cece62f
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2022-01-17 18:07:48 +0530

    Time: 10 ms (46.79%), Space: 43 MB (13.96%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,55 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-//         int n = s.length();
-        
-//         HashSet<Character> set = new HashSet<Character>();
-//         int count = 0;
-//         int[] arr = new int[n];
-        
-//         for (int i=0; i<n; i++) {
-//             if (!set.contains(s.charAt(i))) {
-//                 set.add(s.charAt(i));
-//                 count += 1;
-//             } 
-//             else {
-//                 arr[i] = count;
-//                 count = 0;
-//             }
-//         }
-        
-//         Arrays.sort(arr);
-        
-//         return arr[n-1];
-        
         int n = s.length();
-        int i = 0, j = 0 , len = 0;
-        int[] arr = new int[256];
         
-        while(j < n){
-            
-            int ind = s.charAt(j); 
-            
-            while(arr[ind] == 0){
-                arr[ind]++;
-                j++;
-                if(j < n){
-                    ind = s.charAt(j);
-                }
-                else{
-                    break;
-                }
-               
+        if (s == null || n == 0)
+            return 0;
+        
+        int max = 0, i = 0, j = 0;
+        Set<Character> set = new HashSet<>();
+        
+        while (i < n) {
+            char c = s.charAt(i);
+            while (set.contains(c)) {
+                set.remove(s.charAt(j));
+                ++j;
             }
-            len = Math.max(len , j - i);
-            
-            while(arr[ind] > 0){
-                int temp = s.charAt(i);
-                arr[temp]--;
-                i++;
-                
-            }
-            
+            set.add(c);
+            max = Math.max(max, i - j + 1);
+            ++i;
         }
-        return len;
+        return max;
     }
 }
