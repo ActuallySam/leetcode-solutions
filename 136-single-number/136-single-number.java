b865733f07af5fa438e989fd31a94bf973ad8065
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2022-01-21 01:52:41 +0530

    Time: 27 ms (9.15%), Space: 51.8 MB (8.93%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -5,30 +5,30 @@ class Solution {
     public int singleNumber(int[] nums) {
         
         //HashMap Solution 
-//         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-//         int n = nums.length;
-//         int unique = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int n = nums.length;
+        int unique = 0;
          
-//         for (int i=0; i<n; i++) {
-//             if (!map.containsKey(nums[i]))
-//                 map.put(nums[i], 1);
-//             else
-//                 map.put(nums[i], map.get(nums[i]) + 1);
-//         }
+        for (int i=0; i<n; i++) {
+            if (!map.containsKey(nums[i]))
+                map.put(nums[i], 1);
+            else
+                map.put(nums[i], map.get(nums[i]) + 1);
+        }
         
-//         for (Entry<Integer, Integer> entry: map.entrySet()) {
-//             if (entry.getValue() == 1)
-//                 unique = entry.getKey();
-//         }
-//         return unique;
+        for (Entry<Integer, Integer> entry: map.entrySet()) {
+            if (entry.getValue() == 1)
+                unique = entry.getKey();
+        }
+        return unique;
         
         
         // XOR Operator Solution
-        int n = nums.length;
-        int result = 0;
-        for (int i=0; i<n; i++) {
-            result ^= nums[i];
-        }
-        return result;
+        // int n = nums.length;
+        // int result = 0;
+        // for (int i=0; i<n; i++) {
+        //     result ^= nums[i];
+        // }
+        // return result;
     }
 }
