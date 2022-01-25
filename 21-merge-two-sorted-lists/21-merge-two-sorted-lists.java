b2b2cde3798066c85609704ce57e01b14b54a0e3
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2022-01-25 15:08:56 +0530

    Time: 1 ms (46.55%), Space: 39.6 MB (42.74%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -11,42 +11,42 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         // Recursive Solution
-        if (l1 == null) 
-            return l2;
-        if (l2 == null)
-            return l1;
+//         if (l1 == null) 
+//             return l2;
+//         if (l2 == null)
+//             return l1;
         
-        if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        }
-        else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
-        }
+//         if (l1.val < l2.val) {
+//             l1.next = mergeTwoLists(l1.next, l2);
+//             return l1;
+//         }
+//         else {
+//             l2.next = mergeTwoLists(l1, l2.next);
+//             return l2;
+//         }
         
         
         // Iterative Solution
-//         ListNode head = new ListNode(0);
-//         ListNode temp = head;
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
         
-//         while (l1 != null && l2 != null) {
-//             if (l1.val <= l2.val) {
-//                 temp.next = l1;
-//                 l1 = l1.next;
-//             }
-//             else {
-//                 temp.next = l2;
-//                 l2 = l2.next;
-//             }
-//             temp = temp.next;
-//         }
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
         
-//         if (l1 != null)
-//             temp.next = l1;
-//         else if (l2 != null)
-//             temp.next = l2;
+        if (l1 != null)
+            temp.next = l1;
+        else if (l2 != null)
+            temp.next = l2;
         
-//         return head.next;
+        return head.next;
     }
 }
