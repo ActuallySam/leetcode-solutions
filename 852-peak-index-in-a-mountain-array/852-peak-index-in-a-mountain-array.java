7bed49e42b50473d7818e229a177d91452b3905b
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2022-01-26 03:42:07 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (29.64%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,17 +1,31 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         // Faaltu Method
-        int n = arr.length;
-        int[] nums = new int[n];
-        for (int i=0; i<n; i++) 
-            nums[i] = arr[i];
-        Arrays.sort(nums);
-        int number = nums[n-1];
+//         int n = arr.length;
+//         int[] nums = new int[n];
+//         for (int i=0; i<n; i++) 
+//             nums[i] = arr[i];
+//         Arrays.sort(nums);
+//         int number = nums[n-1];
         
-        for (int i=0; i<n; i++) {
-            if (arr[i] == number)
-                return i;
+//         for (int i=0; i<n; i++) {
+//             if (arr[i] == number)
+//                 return i;
+//         }
+//         return -1;
+        
+        // Binary Search
+        int n = arr.length;
+        int i = 0;
+        int j = n - 1;
+        
+        while (i < j) {
+            int mid = (i + j) / 2;
+            if (arr[mid] < arr[mid + 1])
+                i = mid + 1;
+            else
+                j = mid;
         }
-        return -1;
+        return i;
     }
 }
