6402c096a1b629d0b08afeaf83d6a1fc055f62f4
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2022-01-30 03:49:24 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (5.67%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -11,6 +11,7 @@
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode temp = head;
+        ListNode temp1 = head;
         ListNode even = head;
         ListNode odd = head;
         int count = 0;
@@ -20,18 +21,23 @@ class Solution {
             count++;
         }
         
-        if (count % 2 == 0) {
-            for (int i=1; i<=count/2; i++) {
-                even = even.next;
-            }
-            return even;
-        }
-        else {
-            for (int i=1; i<=count/2; i++) {
-                odd = odd.next;
-            }
-            return odd;
+        for (int i=1; i<=count/2; i++) {
+            temp1 = temp1.next;
         }
         
+        return temp1;
+        
+        // if (count % 2 == 0) {
+        //     for (int i=1; i<=count/2; i++) {
+        //         even = even.next;
+        //     }
+        //     return even;
+        // }
+        // else {
+        //     for (int i=1; i<=count/2; i++) {
+        //         odd = odd.next;
+        //     }
+        //     return odd;
+        // }  
     }
 }
