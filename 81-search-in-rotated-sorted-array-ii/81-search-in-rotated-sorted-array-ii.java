8cc904cde50a1de4b275345cde63a4686a0614b2
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2022-02-02 03:59:43 +0530

    Time: 1 ms (61.20%), Space: 44.4 MB (5.37%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,19 +1,47 @@
 class Solution {
     public boolean search(int[] nums, int target) {
+        // 
         int n = nums.length;
-        Arrays.sort(nums);
-        
-        int i = 0;
-        int j = n - 1;
-        while (i <= j) {
-            int mid = (i + j) / 2;
-            if (nums[mid] == target)
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if (nums[mid] == target) 
                 return true;
-            else if (nums[mid] < target)
-                i = mid + 1;
-            else
-                j = mid - 1;
+            
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target <= nums[mid])
+                    high = mid - 1;
+                else
+                    low = low + 1;
+            }
+            
+            else {
+                if (target >= nums[mid] && target <= nums[high])
+                    low = low + 1;
+                else 
+                    high = mid - 1;
+            }
         }
         return false;
+        
+        
+        // Normal Binary Search
+//         int n = nums.length;
+//         Arrays.sort(nums);
+        
+//         int i = 0;
+//         int j = n - 1;
+//         while (i <= j) {
+//             int mid = (i + j) / 2;
+//             if (nums[mid] == target)
+//                 return true;
+//             else if (nums[mid] < target)
+//                 i = mid + 1;
+//             else
+//                 j = mid - 1;
+//         }
+//         return false;
     }
 }
